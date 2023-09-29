b437fba61a0f89b1710bd0b30b5412c8b45ab6ef
Sun77Jay <godzilllla77@gmail.com>
2023-09-28 23:02:18 -0400

    initial commit

--- javascript/script.js
+++ javascript/script.js
@@ -1,44 +1,19 @@
-const images = document.querySelectorAll('#slider img');
-const previousImage = document.getElementById("prev");
-const nextImage = document.getElementById("next");
-
-let currentIndex = 0;
-
-function reset() {
-  for (let i = 0; i < images.length; i++) {
-    images[i].classList.remove('active');
-  }
+// Write your code below
+const items = document.querySelectorAll('.item:not(:first-child)');
+const options = {
+  threshold: 0.5
 }
 
-function initializeSlider() {
-  reset();
-  images[currentIndex].classList.add('active');
+function addSlideIn(entries) {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add('slide-in');
+    }
+  });
 }
 
-function slideLeft() {
-  reset();
-  currentIndex--;
-  if (currentIndex < 0) {
-    currentIndex = images.length - 1;
-  }
-  images[currentIndex].classList.add('active');
-}
+const observer = new IntersectionObserver(addSlideIn, options)
 
-function slideRight() {
-  reset();
-  currentIndex++;
-  if (currentIndex >= images.length) {
-    currentIndex = 0;
-  }
-  images[currentIndex].classList.add('active');
-}
-
-initializeSlider();
-
-previousImage.addEventListener('click', function() {
-  slideLeft();
-});
-
-nextImage.addEventListener('click', function() {
-  slideRight();
-});
+items.forEach(item => {
+  observer.observe(item);
+})
